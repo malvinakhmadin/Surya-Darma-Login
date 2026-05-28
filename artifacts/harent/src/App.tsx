b054857c5e44944ed237c8503d86d27b81f82825
malvinakhmadin <138467722+malvinakhmadin@users.noreply.github.com>
2026-05-29 00:57:13 +0700

    Update link in App.tsx to new Google Script URL

--- artifacts/harent/src/App.tsx
+++ artifacts/harent/src/App.tsx
@@ -40,7 +40,7 @@ function Home() {
               className="w-full flex justify-center"
             >
               <a 
-                href="https://script.google.com/macros/s/AKfycbx4eV60J-B1oCTyRme61C0UvJXrO3z2XY3UEPiTj11-nSEHg50pysskdPLFt3dPYQHz/exec"
+                href="https://script.google.com/macros/s/AKfycbwZsEl2tSEeqSyx8mwtHat1IMEdqz7GbYT3SAH8eP6OA7822grMWwKNangL0PHJ5dn4/exec"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="group relative inline-flex items-center justify-center gap-1.5 bg-[#1b5df1] hover:bg-[#154ac6] text-white py-2 px-5 rounded-lg font-medium text-sm tracking-wide shadow-[0_2px_10px_rgba(27,93,241,0.35)] transition-all duration-300 hover:shadow-[0_4px_14px_rgba(27,93,241,0.55)] hover:-translate-y-0.5 active:translate-y-0"

cf506f5deacfe34325719ced2780f22a0f5687c5
malvinakhmadin <138467722+malvinakhmadin@users.noreply.github.com>
2026-06-18 14:52:25 +0700

    Update App.tsx

--- artifacts/harent/src/App.tsx
+++ artifacts/harent/src/App.tsx
@@ -40,7 +40,7 @@ function Home() {
               className="w-full flex justify-center"
             >
               <a 
-                href="https://script.google.com/macros/s/AKfycbyyXRd_rpk3NYtTQnuAAVILpuDTxtvJXJcsD1pswsSeu1Pf0EOp3KBIq35qSkIo3HvZ/exec"
+                href="https://script.google.com/macros/s/AKfycbzycOkcSfjvsIH1KtW57bMGIdNKhuCh3BB6qzQWjWO0vJB2lkTO9WTNsqNmOukAVAaL/exec"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="group relative inline-flex items-center justify-center gap-1.5 bg-[#1b5df1] hover:bg-[#154ac6] text-white py-2 px-5 rounded-lg font-medium text-sm tracking-wide shadow-[0_2px_10px_rgba(27,93,241,0.35)] transition-all duration-300 hover:shadow-[0_4px_14px_rgba(27,93,241,0.55)] hover:-translate-y-0.5 active:translate-y-0"
